b595fa2aad459a903339e1273d54a41a2dba9ec7
Lee Crosby <lee.marreros@pucp.pe>
2015-10-23 12:45:29 -0700

    [Save] Implements video and progress screen

--- client/components/phoneScreens/videoReproduction.js
+++ client/components/phoneScreens/videoReproduction.js
@@ -2,6 +2,20 @@
 
 import React from 'react';
 
+var infoComments = [
+  {
+    avatarSrc: '../../img/avatarImage1.png',
+    nameLocation: 'Laura - Mountain View',
+    comment: '“This is and will be one of my favorite Peruvian  recipes. Love the video!”',
+    dishContribution: '../../img/dishContribution1.png'
+  },
+  {
+    avatarSrc: '../../img/avatarImage2.png',
+    nameLocation: 'Stefano - San Franciso',
+    comment: '“I’m glad I’ve found this recipe. Very well explained and easy to make.”',
+    dishContribution: '../../img/dishContribution2.png'
+  }
+];
 
 class VideoReproduction extends React.Component{
   constructor(props) {
@@ -11,10 +25,46 @@ class VideoReproduction extends React.Component{
   }
   render() {
     return (
-      <div className='VideoReproduction'>
-        VideoReproductions
+      <div className='videoReproduction'>
+        <div className='header'>
+          <h1 className='limaC'>LIMA CULINARY</h1>
+          <img src='../../img/miniFireLogo.png'/>
+          <h1 className='videos'>Lomo Saltado</h1>
+        </div>
+        <div className='bodyWrapper'>
+          <div className='contentBody'>
+            <div className='videoProgress'>
+              <div className='progressWrapper'><img src='../../img/progressVideoBar.png'/></div>
+              <div className='steps'>
+                <h1>1. Preparing the French Fries</h1>
+                <h1 className='stepNumber2'>2. Cutting the welsh onion</h1>
+                <h1>3. Cutting and seasoning the beef</h1>
+                <h1>4. Mixing all the ingredients</h1>
+                <h1>5. Serving the food</h1>
+              </div>
+            </div>
+            <div className='commentsWrapper'>
+              {infoComments.map(function(item, i){
+                return (
+                  <div className='itemComment' key={i}>
+                    <div className='contentComment'>
+                      <div className='avatarAndComment'>
+                        <img src={item.avatarSrc}/>
+                        <h1>{item.nameLocation}</h1>
+                      </div>
+                      <h1>{item.comment}</h1>
+                    </div>
+                    <div className='pictureContrib'>
+                      <img src={item.dishContribution}/>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </div>
-      );
+    );
   }
 };
 
