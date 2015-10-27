71cba81cd970a6fd0a00d199ef78db96cf568d67
Lee Crosby <lee.marreros@pucp.pe>
2015-10-27 13:16:44 -0700

    [Feature] Adds host in Firebase and upgrade Modal subscribe

--- client/components/subscribeSection.js
+++ client/components/subscribeSection.js
@@ -2,8 +2,8 @@
 
 import React from 'react';
 import Firebase from 'firebase';
-var ref = new Firebase("https://lima-culinary.firebaseio.com/");
-var userEmail = ref.child("emails");
+var ref = new Firebase('https://lima-culinary.firebaseio.com/');
+var userEmail = ref.child('emails');
 var re = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
 var ENTER_KEY_CODE = 13;
 
@@ -11,9 +11,19 @@ class SubscribeSection extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      text: null,
-      validateEmail: null
+      text: '',
+      validateEmail: null,
+      openedModal: null,
     };
+  }
+
+  _openingModal() {
+    this.setState({openedModal: true});
+  }
+
+  _closingModal() {
+    this.setState({openedModal: false});
+    console.log('closing');
   }
 
   _onChange(event, value) {
@@ -21,7 +31,6 @@ class SubscribeSection extends React.Component{
       console.log(!!re.exec(this.state.text));
       this.setState({validateEmail: !!re.exec(this.state.text)});
     })
-
   }
 
   _onKeyDown(event) {
@@ -32,8 +41,10 @@ class SubscribeSection extends React.Component{
         console.log('Subscribed as: ', text);
         userEmail.push(text);
         this.setState({text: ''});
+        this._openingModal();
       } else if (!re.exec(text)) {
         console.log('wrong email')
+        this._openingModal();
       }
     }
   }
@@ -43,31 +54,54 @@ class SubscribeSection extends React.Component{
         console.log('Subscribed as: ', this.state.text);
         userEmail.push(this.state.text);
         this.setState({text: ''});
+        this._openingModal(true);
       } else if (!re.exec(this.state.text)) {
+        this._openingModal(false);
         console.log('wrong email')
       }
   }
-
+  _closePage() {
+    window.close();
+  }
   render() {
-    console.log(!!this.state.text);
+    var modalToShow = <div className='modalWrapper' onClick={this._closingModal.bind(this)}>
+                        <div className='modalContent'>
+                          <h2 className='exitButton' onClick={this._closingModal.bind(this)}>X</h2>
+                            <div className='modalInfoWrong'>
+                              <div className='wrapperEmail'>
+                                {this.state.validateEmail ?
+                                  <h1 className='validEmail'>Thank you! See you soon!</h1>:
+                                  <h1>Email not valid</h1>}
+                              </div>
+                              <div onClick={this.state.validateEmail ? this._closePage.bind(this) : this._closingModal.bind(this)} className='buttonTryAgain'>
+                                {this.state.validateEmail ?
+                                  <h1>Close page</h1> :
+                                  <h1>Try again</h1>}
+                              </div>
+                            </div>
+                        </div>
+                      </div>;
+
     return (
       <div className='descriptionIdea'>
+        {this.state.openedModal ? <div className='closePopUpWindow' onClick={this._closingModal.bind(this)}></div> : null}
         <div className='wrapperDescription'><h1>Subscribe and we’ll let you know when this is ready:</h1></div>
         <div className='wrapperSubscribeInput'>
           <div className='subscribeInput'>
             <div className='textAreaWrapper' id={!!this.state.text ? (this.state.validateEmail ? 'valid' : 'notValid') : null}>
               <textarea
-                className="emailComposer"
+                className='emailComposer'
                 placeholder='Type here your email here.'
-                name="message"
+                name='message'
                 value={this.state.text}
                 onChange={this._onChange.bind(this)}
                 onKeyDown={this._onKeyDown.bind(this)}/>
             </div>
-            <div className='buttonSubscribe' onClick={this._subscribeButton.bind(this)}>
-              <h1 >Subscribe me!</h1>
+            <div className='buttonSubscribe' onClick={this._subscribeButton.bind(this)} href='#openModal'>
+              <h1>Subscribe me!</h1>
             </div>
           </div>
+            {this.state.openedModal ? modalToShow : null}
         </div>
       </div>
       );
